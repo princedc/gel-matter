86d610621ae09a11b6225a45162693f4e677ccb4
Simon Sinclair <simonsinclair@users.noreply.github.com>
2016-12-16 15:05:08 +0000

    Update Masthead component

--- organisms/Masthead.vue
+++ organisms/Masthead.vue
@@ -1,7 +1,16 @@
 <template>
-	<h1>Hello, World.</h1>
+	<div class="gel-wrap">
+		<div class="gel-layout">
+			<div class="gel-layout__item gel-1/2">HALF</div>
+			<div class="gel-layout__item gel-1/2">HALF</div>
+		</div>
+	</div>
 </template>
 
-<style>
-	
+<script>
+	// ...
+</script>
+
+<style lang="sass">
+	@import '../common';
 </style>
