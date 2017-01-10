6f9a93fa90e7a5d541eda5f2d5d6136161aeff19
Rob Gamm <Rob.Gamm.ext@bbc.co.uk>
2017-01-10 09:55:53 +0000

    Add basic form components

---
+++ atoms/Select.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <label v-bind:for="inputId">{{ label }}</label>
+    <select v-bind:id="inputId"
+            v-bind:value="value"
+            v-on:input="updateValue($event.target.value)"
+    >
+      <option v-for="option in options">{{ option.name }}</option>
+    </select>
+    <div class="gel-break-out-box">
+      <h4>{{ helpText }}</h4>
+      <p v-if="helpTextDetails">{{ helpTextDetails }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["label", "type", "helpText", "helpTextDetails", "errors", "value"],
+    computed: {
+      inputId: function idGen() {
+        return this.label.toLowerCase().replace(/[^\w]/, '');
+      }
+    },
+    methods: {
+      updateValue: function(value) {
+        this.$emit('input', value);
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  @import '../common';
+
+
+</style>
