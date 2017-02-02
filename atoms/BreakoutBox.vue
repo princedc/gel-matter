01f8764b10f03a22cc1b96d5ef98e532ced0af0a
Nikolas Tsouk <tsouk.n@gmail.com>
2017-02-02 10:35:23 +0000

    GELWEB-535: Styling breakout box

--- atoms/BreakoutBox.vue
+++ atoms/BreakoutBox.vue
@@ -1,6 +1,7 @@
 <template>
     <div v-if="helpText" class="gel-break-out-box">
-      <h4><gel-icon class="gel-icon" set="core" icon="info" size="small" />{{ helpText }}</h4>
+      <gel-icon class="gel-icon gel-icon--inline" set="core" icon="info" size="small" />
+      <h4>{{ helpText }}</h4>
       <p v-if="helpTextDetails">{{ helpTextDetails }}</p>
     </div>
 </template>
@@ -18,13 +19,33 @@
   @import '../common';
 
   .gel-break-out-box {
-    background: #ccc;
+    background: #dcdcdc;
     padding: 8px;
     width: 100%;
     @include gel-typography('brevier');
 
+    h4 {
+      overflow: hidden;
+      margin: 0px;
+      font-weight: normal;
+      @include gel-typography('long-primer');
+    }
+
+    p {
+      margin: 8px 0px 0px 0px;
+    }
+
     .gel-icon {
+      float: left;
+      display: inline;
       margin: 0px 8px 0px 0px;
+    }
+
+    .gel-icon--inline {
+      width: 16px;
+      height: 16px;
+      position: relative;
+      top: 2px;
     }
   }
 </style>
