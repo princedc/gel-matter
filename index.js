8a71942a6f35c2dc0c4745c185d98118ea6c80c5
Simon Sinclair <simonsinclair@users.noreply.github.com>
2017-02-24 15:22:06 +0000

    Flatten exports from root module

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-import atoms from './atoms';
-import molecules from './molecules';
-import organisms from './organisms';
-
-export { atoms, molecules, organisms };
+export * from './atoms';
+export * from './molecules';
