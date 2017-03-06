106bebf3b3f52d9d2b34329912bef6de0d08e769
Simon Sinclair <simonsinclair@users.noreply.github.com>
2017-03-06 16:24:38 +0000

    Update docs

--- docs/templates/Index.vue
+++ docs/templates/Index.vue
@@ -1,13 +1,38 @@
 <template>
-  <h1>Documentation</h1>
+  <div class="gel-wrap">
+    <div class="gel-layout">
+      <div class="gel-layout__item">
+
+          <h1>GEL Matter</h1>
+          <ul>
+            <li><a @click.prevent="currentView = 'Atoms'" href="#">Atoms</a></li>
+            <li><a @click.prevent="currentView = 'Molecules'" href="#">Molecules</a></li>
+            <li><a @click.prevent="currentView = 'Organisms'" href="#">Organisms</a></li>
+          </ul>
+
+          <keep-alive>
+            <component :is="currentView"></component>
+          </keep-alive>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-  export default {
+  import Atoms from '../views/Atoms.vue';
+  import Molecules from '../views/Molecules.vue';
+  import Organisms from '../views/Organisms.vue';
 
+  export default {
+    components: { Atoms, Molecules, Organisms },
+    data() {
+      return {
+        currentView: 'Atoms',
+      };
+    },
   };
 </script>
 
 <style lang="scss">
-  @import '../../src/common';
+  // ...
 </style>
---
+++ docs/views/Molecules.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Molecules</h2>
+</template>
